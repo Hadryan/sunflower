db9690813bc00ed0f26cb15a4eaf26556f3b60af
jbeisel <jbeisel@hevaweb.com>
2020-08-10 13:49:21 +0200

    Minor edits

--- sunflower/song.py
+++ sunflower/song.py
@@ -10,9 +10,9 @@ class Song:
         self.sr = None
 
         self.load_track(path)
+        self.process_song()
 
         # Features
-
         self.tempo = None
         self.beat_frames = None
 
@@ -21,7 +21,14 @@ class Song:
     def load_track(self, path):
         """Loads a song based on a file path.
         """
+
         self.waveform, self.sr = librosa.load(path)
+
+    def process_song(self):
+        """Removes silence at the beginning of the song.
+        """
+
+        self.waveform, _ = librosa.effects.trim(self.waveform)
 
     def detect_tempo(self):
         """Detects tempo of a track.
@@ -30,10 +37,13 @@ class Song:
         if (self.sr is None) or (self.waveform is None):
             raise ValueError("No song was loaded.")
 
+        # Detect tempo
         self.tempo, self.beat_frames = librosa.beat.beat_track(
             y=self.waveform, sr=self.sr
         )
 
+        
 
-beat = Song("data/mirage.mp3")
-print(beat.tempo)
+
+beat = Song("data/examplesong.wav")
+print(round(beat.tempo, 0))
